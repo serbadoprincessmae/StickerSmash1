a0fb2f7ad8765f48be103747e11dc0f951f066eb
jemima_elemino <jemimaelemino674@gmail.com>
2026-07-08 00:22:50 -0700

    add 2nd number

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>About screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#d6cdd8',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
